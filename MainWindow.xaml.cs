c078d99a9caecc4f4f801f4b744d06f7d28da91d
Doublemoses <mjacobs010@hotmail.com>
2025-08-19 02:31:43 +1000

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NightreignSeedUtility;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    SeedUtility seedUtility;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        seedUtility = new SeedUtility();
+        InitUI();
+    }
+
+    private void InitUI()
+    {
+        listviewStoreItems.ItemsSource = seedUtility.storeItems;
+        FilterStoreItems();
+    }
+
+    private void Button_GetStore(object sender, RoutedEventArgs e)
+    {
+        //MessageBox.Show(seedUtility.GetStoreFromSeed(0x150CB06E).ToString());
+
+        if (textboxSeed.Text.Length == 0)
+            return;
+
+        uint storeId = 0;
+
+        try
+        {
+            storeId = seedUtility.GetStoreFromSeed(Convert.ToUInt32(textboxSeed.Text, 16));
+        }
+        catch
+        {
+            MessageBox.Show("Invalid data in seed textbox.");
+            return;
+        }
+        
+        FilterStoreItems(storeId);
+
+        textShopId.Text = "Store Id: " + storeId.ToString();
+    }
+
+    private void FilterStoreItems(uint storeId = 10000)
+    {
+        ICollectionView view = CollectionViewSource.GetDefaultView(seedUtility.storeItems);
+
+        view.Filter = (entry) =>
+        {
+            StoreItem item = (StoreItem)entry;
+            uint general = 0;
+
+            switch (storeId - 90000)
+            {
+                case 1000:
+                    break;
+                case < 7:
+                    general = 100;
+                    break;
+                case < 14:
+                    general = 200;
+                    break;
+                case < 21:
+                    general = 300;
+                    break;
+            }
+
+            return item.Store == storeId || item.Store == general;
+        };
+    }
+
+    private void TextBox_VerifySeed(object sender, TextCompositionEventArgs e)
+    {
+        foreach (char c in e.Text)
+        {
+            if (!char.IsAsciiHexDigit(c))
+            {
+                e.Handled = true;
+                break;
+            }
+        }
+    }
+}
---
+++ SeedUtility.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace NightreignSeedUtility
+{
+    class SeedUtility
+    {
+        public SeedUtility()
+        {
+            LoadStoreItems();
+        }
+
+        const long factor = 0x5deece66d;
+
+        public uint GetStoreFromSeed(uint seed)
+        {
+            uint[] important = new uint[7];
+
+            long a_long = seed * factor + 0xB;
+
+            for (int x = 0; x < 4; x++)
+            {
+                important[x] = (uint)(a_long >>> 0x20);
+                a_long = a_long * factor + 0xB;
+
+                Debug.Print("important[" + x + "] " + important[x].ToString("X"));
+            }
+
+            uint a_int = important[0] << 0xB;
+            important[4] = a_int ^ important[0];
+
+            Debug.Print("important[4] " + important[4].ToString("X"));
+
+            a_int = important[3] >>> 0xB;
+            a_int = a_int ^ important[4];
+            a_int = a_int >>> 8;
+            a_int = a_int ^ important[3];
+            important[5] = a_int ^ important[4];
+
+            Debug.Print("important[5] " + important[5].ToString("X"));
+
+            important[6] = (important[1] << 0xB) ^ important[1];
+
+            a_int = important[5] >>> 0xB;
+            a_int = a_int ^ important[6];
+            a_int = a_int >>> 8;
+            a_int = a_int ^ important[5];
+            a_int = a_int ^ important[6];
+
+            Debug.Print("a int " + a_int.ToString() + " hex " + a_int.ToString("X"));
+
+            uint answer = (a_int % 0x15);
+
+            Debug.Print(answer.ToString());
+
+            return 90000 + (a_int % 0x15);
+
+        }
+
+        public List<StoreItem> storeItems = new List<StoreItem>();
+
+        public void LoadStoreItems()
+        {
+            try
+            {
+                string line;
+                var assembly = Assembly.GetExecutingAssembly();
+                string resource = Assembly.GetExecutingAssembly().GetManifestResourceNames().Single(str => str.EndsWith("items.tsv"));
+                using (StreamReader sr = new StreamReader(assembly.GetManifestResourceStream(resource)))
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] split = line.Split("\t");
+
+                        if (split.Length == 7)
+                        {
+                            StoreItem item = new StoreItem();
+
+                            item.Store = UInt32.Parse(split[0]);
+                            item.Name = split[1];
+                            item.Passive = split[2];
+                            item.Skill = split[3];
+                            item.Spell1 = split[4];
+                            item.Spell2 = split[5];
+                            item.Price = UInt32.Parse(split[6]);
+
+                            storeItems.Add(item);
+                        }
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+    }
+
+    class StoreItem
+    {
+        public uint Store { get; set; }
+        public string Name { get; set; }
+        public string Passive { get; set; }
+        public string Skill { get; set; }
+        public string Spell1 { get; set; }
+        public string Spell2 { get; set; }
+        public uint Price { get; set; }
+
+        public StoreItem()
+        {
+            Name = "";
+            Passive = "";
+            Skill = "";
+            Spell1 = "";
+            Spell2 = "";
+        }
+
+        public string DisplaySkill
+        {
+            get
+            {
+                if (this.Skill == "" && this.Spell1 != "")
+                    return Spell1 + " / " + Spell2;
+                return this.Skill;
+            }
+        }
+    }
+}
